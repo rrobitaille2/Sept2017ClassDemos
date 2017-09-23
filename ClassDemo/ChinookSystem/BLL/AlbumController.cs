2327dba9a70c4dd030464324ef889ac1fd43d8d9
Don Welch <dwelch@nait.ca>
2017-09-22 20:57:00 -0600

    Implement First Linq Sample

---
+++ ClassDemo/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;  //expose methods for ODS wizard
+using Chinook.Data.POCOs;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<ArtistAlbumByReleaseYear> Albums_ListforArtist(int artistid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from x in context.Albums
+                              where x.ArtistId.Equals(artistid)
+                              select new ArtistAlbumByReleaseYear
+                              {
+                                  Title = x.Title,
+                                  Released = x.ReleaseYear
+                              };
+                return results.ToList();
+            }
+        }
+    }
+}
