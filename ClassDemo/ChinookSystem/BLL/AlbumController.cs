29642d279d97e7adc0759aa8e4fcbe11851e6b68
Don Welch <dwelch@nait.ca>
2017-09-25 14:47:04 -0600

    another linq implementation into vs solution

--- ClassDemo/ChinookSystem/BLL/AlbumController.cs
+++ ClassDemo/ChinookSystem/BLL/AlbumController.cs
@@ -30,6 +30,19 @@ namespace ChinookSystem.BLL
                               };
                 return results.ToList();
             }
+        }//eom
+
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<Album> Albums_FindByYearRange(int minyear, int maxyear)
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from x in context.Albums
+                              where x.ReleaseYear >= minyear && x.ReleaseYear <= maxyear
+                              orderby x.ReleaseYear, x.Title
+                              select x;
+                return results.ToList();
+            }
         }
     }
 }
