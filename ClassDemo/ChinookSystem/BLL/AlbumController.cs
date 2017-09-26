4113384dd6daf1883b095ae40e004725fcb0ca0f
Don Welch <dwelch@nait.ca>
2017-09-25 18:06:04 -0600

    reset for night class

--- ClassDemo/ChinookSystem/BLL/AlbumController.cs
+++ ClassDemo/ChinookSystem/BLL/AlbumController.cs
@@ -32,17 +32,6 @@ namespace ChinookSystem.BLL
             }
         }//eom
 
-        [DataObjectMethod(DataObjectMethodType.Select,false)]
-        public List<Album> Albums_FindByYearRange(int minyear, int maxyear)
-        {
-            using (var context = new ChinookContext())
-            {
-                var results = from x in context.Albums
-                              where x.ReleaseYear >= minyear && x.ReleaseYear <= maxyear
-                              orderby x.ReleaseYear, x.Title
-                              select x;
-                return results.ToList();
-            }
-        }
+      
     }
 }
