d12155ae741104220c1652f38cfe62e6d3e836a4
Don Welch <dwelch@nait.ca>
2017-10-25 19:43:40 -0600

    moving of security class and refacting

---
+++ ClassDemo/Chinook.Data/Entities/Security/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Data.Entities.Security
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int? EmployeeID { get; set; }
+        public int? CustomerID { get; set; }
+    }
+}
